6d2615a04804cff9b22c8f2308e12c01383c544d
ju-voro <julia.voronetskaja@tktk.ee>
2023-01-02 21:21:03 +0100

    number to string

---
+++ numberToString.js
@@ -0,0 +1,10 @@
+//We need a function that can transform a number (integer) into a string.
+
+let num = 123;
+
+console.log(typeof(numberToString(num)));
+
+function numberToString(num) {
+  return num.toString();
+}
+
