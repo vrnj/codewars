ff62eab53907fbeea9765456c2a4e895ea3cc2a3
ju-voro <voronetskajaj@hotmail.com>
2023-01-03 20:45:30 +0100

    string to int by casting Number

--- stringToNumber.js
+++ stringToNumber.js
@@ -4,3 +4,9 @@ const stringToNumber = function(str){
   // put your code here
   return parseInt(str);
 }
+
+//casting ->
+// var stringToNumber = function(str){
+//   // put your code here
+//   return Number(str);
+// }
