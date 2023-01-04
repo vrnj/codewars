5a2127f9b4fac639816ba260688139d985b7b32b
ju-voro <voronetskajaj@hotmail.com>
2023-01-04 09:29:14 +0100

    middle letters of a string

---
+++ lettersInTheMiddle.js
@@ -0,0 +1,16 @@
+
+
+let s = 'testing';
+
+console.log(getMiddle(s));
+
+function getMiddle(s)
+{
+  //Code goes here!
+  if(s.length % 2 === 0){
+    return s[s.length/2-1] + s[s.length/2];
+  }
+  else {
+    return s[Math.floor(s.length/2)];
+  }
+}
