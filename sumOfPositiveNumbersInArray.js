f26d4cf318c88c660c84d48c6d94a8b0707639fd
ju-voro <voronetskajaj@hotmail.com>
2023-01-04 09:53:19 +0100

    sum of positives with reduce in comments

--- sumOfPositiveNumbersInArray.js
+++ sumOfPositiveNumbersInArray.js
@@ -1,3 +1,4 @@
+//You get an array of numbers, return the sum of all of the positives ones.
 
 
 function positiveSum(arr) {
@@ -10,3 +11,8 @@ function positiveSum(arr) {
   
   return sum;
 }
+
+//getting clever with reduce
+// function positiveSum(arr) {
+//    return arr.reduce((a,b)=> a + (b > 0 ? b : 0),0);
+// }
