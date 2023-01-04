0a10f077778a4b546f83c3d394176228d06b5a3b
ju-voro <voronetskajaj@hotmail.com>
2023-01-04 09:42:45 +0100

    middle letters of a string with slice in comment

--- lettersInTheMiddle.js
+++ lettersInTheMiddle.js
@@ -1,3 +1,8 @@
+//You are going to be given a word. 
+//Your job is to return the middle character of the word. 
+//If the word's length is odd, return the middle character. 
+//If the word's length is even, return the middle 2 characters.
+
 
 
 let s = 'testing';
@@ -14,3 +19,9 @@ function getMiddle(s)
     return s[Math.floor(s.length/2)];
   }
 }
+
+//best practice with slice
+// function getMiddle(s)
+// {
+//   return s.slice((s.length-1)/2, s.length/2+1);
+// }
