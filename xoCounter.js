addf52d311897412d7381708c60459a2995461d9
ju-voro <voronetskajaj@hotmail.com>
2023-01-05 16:30:05 +0100

    xo counter net solution

--- xoCounter.js
+++ xoCounter.js
@@ -23,3 +23,14 @@ function XO(str) {
     let result = countX === countO ? true: false;
     return result;
 }
+
+//neat solution
+
+// function XO(str) {
+//   var sum = 0;
+//   for(var i=0; i<str.length; i++){
+//     if(str[i].toLowerCase() == 'x') sum++;
+//     if(str[i].toLowerCase() == 'o') sum--;
+//   }
+//   return sum == 0;
+// }
