f90827454ce408282e21236cf0dfb26b9735808e
ju-voro <voronetskajaj@hotmail.com>
2023-01-06 19:09:06 +0100

    initials with map

--- initials.js
+++ initials.js
@@ -6,3 +6,8 @@ function abbrevName(name){
     return `${splitted[0][0].toUpperCase()}.${splitted[1][0].toUpperCase()}`;
 
 }
+
+//with map
+// function abbrevName(name){
+//     return name.split(' ').map(i => i[0].toUpperCase()).join('.')
+// }
