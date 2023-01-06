9b5e4821276a6952138a166c8cfdeff813841af9
ju-voro <voronetskajaj@hotmail.com>
2023-01-06 19:01:00 +0100

    counting likes with switch

--- likes.js
+++ likes.js
@@ -23,3 +23,14 @@ function likes(names) {
   return myString;
   
 }
+
+// function likes(names) {
+//   names = names || [];
+//   switch(names.length){
+//     case 0: return 'no one likes this'; break;
+//     case 1: return names[0] + ' likes this'; break;
+//     case 2: return names[0] + ' and ' + names[1] + ' like this'; break;
+//     case 3: return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this'; break;
+//     default: return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
+//   }
+// }
