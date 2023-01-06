7fa639df697355d0034c01ad2a964ffa3a111bb9
ju-voro <voronetskajaj@hotmail.com>
2023-01-06 11:16:37 +0100

    eight and nine elegant

--- eightAndNine.js
+++ eightAndNine.js
@@ -3,3 +3,8 @@
 function simpleMultiplication(number) {
   return number % 2 === 0 ? number * 8: number * 9;
 }
+
+//even more elegant
+// function simpleMultiplication(n) {
+//     return n * (n % 2 ? 9 : 8);
+// }
