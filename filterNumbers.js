40ad90e024678b9fa31adbf3d829e8c8eedeffdf
ju-voro <voronetskajaj@hotmail.com>
2023-01-07 10:19:34 +0100

    filter strings out

---
+++ filterNumbers.js
@@ -0,0 +1,16 @@
+//In this kata you will create a function 
+//that takes a list of non-negative integers and strings and returns a new list 
+//with the strings filtered out.
+
+function filter_list(l) {
+  let numArr = [];
+  
+  l.forEach(element => {
+    if(typeof element !== 'string' ){
+      numArr.push(element);
+    }
+  });
+  
+  return numArr;
+  // Return a new array with the strings filtered out
+}
