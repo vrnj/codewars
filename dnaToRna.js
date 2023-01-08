73d7014dc25437bc6396acf40e7a0320805206b5
ju-voro <voronetskajaj@hotmail.com>
2023-01-08 20:03:48 +0100

    dna to rna

---
+++ dnaToRna.js
@@ -0,0 +1,9 @@
+let dna = "UTTT";
+
+function DNAtoRNA(dna) {
+  // create a function which returns an RNA sequence from the given DNA sequence
+  
+  return dna.replaceAll('T', 'U');
+}
+
+console.log(DNAtoRNA(dna));
