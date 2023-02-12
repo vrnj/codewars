80a4083c6c392df7a4a57a3b2c68ff347af86928
ju-voro <voronetskajaj@hotmail.com>
2023-02-12 12:17:55 +0100

    object propertz by key

---
+++ objectLookup.js
@@ -0,0 +1,33 @@
+function greet(language) {
+  
+  const db = {
+        english: 'Welcome',
+        czech: 'Vitejte',
+        danish: 'Velkomst',
+        dutch: 'Welkom',
+        estonian: 'Tere tulemast',
+        finnish: 'Tervetuloa',
+        flemish: 'Welgekomen',
+        french: 'Bienvenue',
+        german: 'Willkommen',
+        irish: 'Failte',
+        italian: 'Benvenuto',
+        latvian: 'Gaidits',
+        lithuanian: 'Laukiamas',
+        polish: 'Witamy',
+        spanish: 'Bienvenido',
+        swedish: 'Valkommen',
+        welsh: 'Croeso'
+    }
+  
+  for(let name in db) {
+    if(name === language){
+        return db[name];
+    }
+}
+
+return 'Welcome';
+  
+}
+
+console.log(greet('dutch'));
